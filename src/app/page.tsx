e7d107b4eebeb6baa95aa25ca9b146c8aa149592
Ayush <65385476+iim-ayush@users.noreply.github.com>
2023-03-11 02:24:03 +0530

    Some Files name changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,33 @@
 "use client";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  StarsCanvas,
+  Tech,
+  Works,
+} from "@/components";
+import React from "react";
 
 export default function Home() {
-  return <main className="relative z-0 bg-primary"></main>;
+  return (
+    <main className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </main>
+  );
 }
