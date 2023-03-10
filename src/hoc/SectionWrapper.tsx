164bd041d23f98e8a76a9b8eb203ef6fd637aeb9
Ayush <65385476+iim-ayush@users.noreply.github.com>
2023-03-10 23:06:21 +0530

    Change File Structure to TS

--- src/hoc/SectionWrapper.tsx
+++ src/hoc/SectionWrapper.tsx
@@ -1,19 +1,20 @@
 import { motion } from "framer-motion";
+import React from "react";
 
-import { styles } from "../styles";
+import { styles } from "../utils/styles";
 import { staggerContainer } from "../utils/motion";
 
-const StarWrapper = (Component, idName) =>
+const StarWrapper = (Component: React.ElementType, idName: string) =>
   function HOC() {
     return (
       <motion.section
-        variants={staggerContainer()}
-        initial='hidden'
-        whileInView='show'
+        variants={staggerContainer(1, 1)}
+        initial="hidden"
+        whileInView="show"
         viewport={{ once: true, amount: 0.25 }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
       >
-        <span className='hash-span' id={idName}>
+        <span className="hash-span" id={idName}>
           &nbsp;
         </span>
 
